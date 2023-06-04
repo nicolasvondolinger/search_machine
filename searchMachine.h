3fc87a07db96c7c24fc40ccac539905aa26676fe
lucascassio <lccosta2604@gmail.com>
2023-06-04 13:03:06 -0300

    contrato atualizado

---
+++ searchMachine.h
@@ -0,0 +1,32 @@
+#ifndef searchMachine
+#define searchMachine
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+using std::string;
+using std::map;
+using std::vector;
+
+class SearchMachine {
+    public:
+    // Construtor da Classe;
+    SearchMachine(string documentsPath);
+
+    // Subsistema de indexação
+    void buildIndex(string documentsPath);
+    
+    // Subsistema de recuperação
+    vector<string> search(string);
+
+    private:
+    // Normaliza as palavras (coloca por padrao todas minúsculas)
+    string normalizeWord(string word);
+
+    // Indice invertido
+    map<string, map<string, int>> invertedIndex;
+};
+
+#endif
