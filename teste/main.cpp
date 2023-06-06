44b7a5fb46c7427940ca8f3a81468c348e2c361b
lucascassio <lccosta2604@gmail.com>
2023-06-06 17:11:02 -0300

    teste

--- teste/main.cpp
+++ teste/main.cpp
@@ -4,8 +4,14 @@
 #include <filesystem>
 #include <sstream>
 #include <iostream>
+#include <map>
 
 using namespace std;
+
+map<string, map<string, int>> buildIndex(string newWord, string arquivo, map<string, map<string, int>> invertedIndex_){
+    invertedIndex_[newWord][arquivo]++;
+    return invertedIndex_;
+}
 
 
 string normalizeWord(string word) {
@@ -21,7 +27,8 @@ string normalizeWord(string word) {
     return normalized;
 }
 
-void readFile() {
+map<string, map<string, int>> readFile() {
+    map<string, map<string, int>> invertedIndex_;
     for (const auto arquivo : filesystem::directory_iterator("./documentos/")) {
         if (arquivo.is_regular_file()) {
             ifstream arquivoEntrada(arquivo.path());
@@ -33,14 +40,31 @@ void readFile() {
                     while (iss >> palavra) {
                         string fileName = arquivo.path().stem();
                         string newWord = normalizeWord(palavra);
-                        
-                        cout << arquivo << endl;
+                        invertedIndex_ = buildIndex(newWord, fileName, invertedIndex_);
                     }
                 }
                 arquivoEntrada.close();
             }
         }
     }
+    return invertedIndex_;
+}
+
+
+string final (map<string, int> mapaInterno) {
+    string final  = "(";
+    for(auto it = mapaInterno.begin(); it != mapaInterno.end(); it++) {
+        final += it -> first;
+        int frequencia = it -> second;
+        final += ", " + to_string(frequencia);
+        if(it++ == mapaInterno.end()){
+            final += ")";
+            break;
+        }
+        final += "), (";
+    }
+
+    return final;
 }
 
 
@@ -48,6 +72,21 @@ int main() {
 
 
     readFile();
+
+    map<string, map<string, int>> invertedIndex_ = readFile();
+
+    for(auto it = invertedIndex_.begin(); it != invertedIndex_.end(); it++) {
+        string palavra = it -> first;
+        map<string, int> mapaInterno = it->second;
+        for(auto it2 = mapaInterno.begin(); it2 != mapaInterno.end(); it2++) {
+            string arquivo = it2 -> first;
+            int frequencia = it2 -> second;
+            string valor = final(mapaInterno);
+            cout << palavra << " " << valor << endl;
+            break;
+        }
+    }
+
 
     return 0;
 }
